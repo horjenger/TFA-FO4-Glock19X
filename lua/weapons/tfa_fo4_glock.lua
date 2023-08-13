fd470b48af85d8e501bd72a5be732a77a40d54ae
horjenger <bagdanmad@gmail.com>
2023-08-13 19:41:16 +0400

    Rebalance

--- lua/weapons/tfa_fo4_glock.lua
+++ lua/weapons/tfa_fo4_glock.lua
@@ -65,7 +65,7 @@ SWEP.Primary.KickDown = 0.2 -- This is the maximum downwards recoil (skeet)
 SWEP.Primary.KickHorizontal = 0.14 -- This is the maximum sideways recoil (no real term)
 SWEP.Primary.StaticRecoilFactor = 0.1 --Amount of recoil to directly apply to EyeAngles.  Enter what fraction or percentage (in decimal form) you want.  This is also affected by a convar that defaults to 0.5.
 --Firing Cone Related
-SWEP.Primary.Spread = .016 --This is hip-fire acuracy.  Less is more (1 is horribly awful, .0001 is close to perfect)
+SWEP.Primary.Spread = .012 --This is hip-fire acuracy.  Less is more (1 is horribly awful, .0001 is close to perfect)
 SWEP.Primary.IronAccuracy = .008 -- Ironsight accuracy, should be the same for shotguns
 --Unless you can do this manually, autodetect it.  If you decide to manually do these, uncomment this block and remove this line.
 SWEP.Primary.SpreadMultiplierMax = 4--How far the spread can expand when you shoot. Example val: 2.5
